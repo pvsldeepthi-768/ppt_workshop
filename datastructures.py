a13e4ccb90ec3e9f5a5a1cb350bafeaa15602d05
P.V.S.L.Deepthi <161279349+pvsldeepthi-768@users.noreply.github.com>
2025-12-09 10:45:55 +0530

    Add datastructures.py to demonstrate data types

---
+++ datastructures.py
@@ -0,0 +1,17 @@
+numbers=[]
+names=()
+roll={23}
+marks={}
+print("Types:")
+print(f"Type of numbers:{type(numbers)}")
+print(f"Type of names:{type(names)}")
+print(f"Type of roll:{type(roll)}")
+print(f"Type of marks:{type(marks)}")
+"""
+output:
+Types:
+Type of numbers:<class 'list'>
+Type of names:<class 'tuple'>
+Type of roll:<class 'set'>
+Type of marks:<class 'dict'>
+"""
